f14617c7c694dea71cad0decc292b74b9cf92f45
Hey <h1017760@connect.hku.hk>
2015-06-20 16:22:57 +0800

    MessageBoard: Updated board.html

--- ui/js/board.js
+++ ui/js/board.js
@@ -1,3 +1,12 @@
 /**
  * Created by Hey on 20/6/15.
  */
+var app = angular.module('myApp', ['freeDragger']);
+app.controller('messagesCtrl', function ($scope, $http) {
+    $http.get("http://172.30.46.130:3000/user/" + $scope.user.name + "/notes")
+        .success(function (response) {
+            $scope.messageJSON = response;
+
+
+        });
+});
