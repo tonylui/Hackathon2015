292c4fbd91c651377070d03fec1357a8ed668172
Hey <h1017760@connect.hku.hk>
2015-06-20 16:54:56 +0800

    MessageBoard: Updated board.html

--- ui/js/board.js
+++ ui/js/board.js
@@ -1,9 +1,12 @@
 /**
  * Created by Hey on 20/6/15.
  */
+//var SERVER_URL = "http://172.30.46.130:3000";
+var SERVER_URL = "http://52.74.226.158:3000";
+
 var app = angular.module('myApp', ['freeDragger']);
 app.controller('messagesCtrl', function ($scope, $http) {
-    $http.get("http://172.30.46.130:3000/user/" + $scope.user.name + "/notes")
+    $http.get(SERVER_URL+"/user/" + $scope.user.name + "/notes")
         .success(function (response) {
             $scope.messageJSON = response;
 
